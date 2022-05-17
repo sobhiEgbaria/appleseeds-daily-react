69a3b4641a50cf83817675a7fe68a9470b90cc6d
sobhiEgbaria <sobhim686@gmail.com>
2022-05-17 18:04:18 +0300

    fix code in 7.2

--- src/ex/ex7.2.jsx
+++ src/ex/ex7.2.jsx
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 
-class ex72 extends React.Component {
-  data = {
-    text: "increment",
-    num: 0,
-  };
+export default class ex72 extends Component {
+  state = { isHidden: false };
 
-  change = () => {
-    this.setState({ num: this.data.num++ });
+  hideShow = () => {
+    this.setState((prev) => {
+      return { isHidden: !prev.isHidden };
+    });
   };
 
   render() {
     return (
       <>
-        <button onClick={this.change}>{this.data.text}</button>
-        <label htmlFor="">{this.data.num}</label>
+        <div
+          className="box"
+          style={{
+            width: "300px",
+            height: "300px",
+            border: "1px solid black",
+            padding: "10px",
+          }}
+        >
+          <button onClick={this.hideShow}>click</button>
+          {this.state.isHidden && (
+            <div
+              className="inner-box"
+              style={{
+                width: "150px",
+                height: "150px",
+                backgroundColor: "red",
+                margin: "10px",
+              }}
+            ></div>
+          )}
+        </div>
       </>
     );
   }
 }
-
-export default ex72;
---
+++ src/ex/ex7.3.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class ex73 extends React.Component {
+  state = {};
+  render() {
+    return null;
+  }
+}
+
+export default ex73;
